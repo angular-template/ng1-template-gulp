7c882d7a08323ddc5b8d6e494913d8ed30ea29dc
Jeevan James <jeevanjj@yahoo.com>
2016-07-29 04:24:33 +0530

    Exporting utils from package. Temp fix for issue caused by firstJs config value.

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 
 let config = require('./lib/config');
 
+let utils = {
+    createModule: require('./lib/utils/create-module')
+};
+
 // let requireDir = require('require-dir');
 // requireDir('./lib/tasks', { recurse: true } );
 
 module.exports = {
-    config: config
+    config: config,
+    utils: utils
 };

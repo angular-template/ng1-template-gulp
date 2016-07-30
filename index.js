35af176afbcfcd81f02588dc55c0544d44501edf
Jeevan James <jeevanjj@yahoo.com>
2016-07-31 03:22:21 +0530

    Completed migration to the package.

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 'use strict';
 
-let config = require('./lib/config');
-
-let utils = {
-    createModule: require('./lib/utils/create-module')
-};
-
-// let requireDir = require('require-dir');
-// requireDir('./lib/tasks', { recurse: true } );
+let requireDir = require('require-dir');
+requireDir('./lib/tasks', { recurse: true } );
 
 module.exports = {
-    config: config,
-    utils: utils
+    config: require('./lib/config'),
+    utils: require('./lib/utils')
 };
